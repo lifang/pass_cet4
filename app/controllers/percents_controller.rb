017dd021aaf179ac7b50a0d347c660ece3179059
lifang <santhe_2004@126.com>
2012-03-27 16:58:58 +0800

    add percents

--- app/controllers/percents_controller.rb
+++ app/controllers/percents_controller.rb
@@ -1,6 +1,12 @@
 # encoding: utf-8
 class PercentsController < ApplicationController
-  def index
-    
+  
+  def create
+    total_score = params["ability"].to_i + params["heart"].to_i + params["attitude"].to_i
+    @message = Constant::SCORE_LEVEL[total_score]
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,8 @@ PassCet4::Application.routes.draw do
   # This route can be invoked with purchase_url(:id => product.id)
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
+  resources :percents
+  
   # Sample resource route with options:
   #   resources :products do
   #     member do
@@ -48,7 +48,7 @@ PassCet4::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'percents#index'
 
   # See how all your routes lay out with "rake routes"
 
