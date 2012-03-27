daa9459ed5ba327037e0c026c1f342fd8aa7afbc
xuqiyong <jeffrey6052@163.com>
2012-03-27 16:18:08 +0800

    jeffrey

--- app/controllers/percents_controller.rb
+++ app/controllers/percents_controller.rb
@@ -1,6 +1,29 @@
 # encoding: utf-8
 class PercentsController < ApplicationController
-  def index
-    
+
+
+  #人人测试应用访问路径 http://apps.renren.com/passcet   ,注: localhost:3000启动项目
+
+  @@renren_client_id = "185877"
+  @@renren_secret_key = "a45567526a374dcb9c06412cb3c93d74"
+
+  #人人应用，嵌入首页
+  def renren
+    @client_id = @@renren_client_id
+    render :layout=>false
   end
+
+  #人人oauth2,回调地址中的"#"，替换为"?"
+  def renren_url_generate
+    render :inline=>"<script type='text/javascript'>var p = window.location.href.split('#');var pr = p.length>1 ? p[1] : '';window.location.href = '/percents/check?'+pr;</script>"
+  end
+
+  #人人登录之后的回调页面，取得用户信息
+  def check
+    access_token = params["access_token"]
+    cookies[:access_token] = access_token
+    user_info = renren_get_user(access_token,@@renren_secret_key)
+    render :inline=>"#{user_info}"
+  end
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,42 @@
 # encoding: utf-8
 module ApplicationHelper
+
+  require 'net/http'
+  #START -----人人API
+  #人人主方法
+  def renren_api(request)
+    uri = URI.parse("http://api.renren.com")
+    http = Net::HTTP.new(uri.host, uri.port)
+    response = http.request(request).body
+  end
+  #
+  #构成人人签名请求
+  def renren_sig_request(query,secret_key)
+    str = ""
+    query.sort.each{|key,value|str<<"#{key}=#{value}"}
+    str<<secret_key
+    sig = Digest::MD5.hexdigest(str)
+    query[:sig]=sig
+    request = Net::HTTP::Post.new("/restserver.do")
+    request.set_form_data(query)
+    return request
+  end
+  #
+  #人人获取用户信息
+  def renren_get_user(access_token,secret_key)
+    query = {:access_token => access_token,:format => 'JSON',:method => 'xiaonei.users.getInfo',:v => '1.0'}
+    request = renren_sig_request(query,secret_key)
+    response = JSON renren_api(request)
+  end
+  #
+  #人人发送新鲜事
+  def renren_send_message(access_token,message,secret_key)
+    query = {:access_token => "#{access_token}",:comment=>"#{message}",:format => 'JSON',:method => 'share.share',:type=>"6",:url=>"http://www.gankao.co",:v => '1.0'}
+    request = renren_sig_request(query,secret_key)
+    response =JSON renren_api(request)
+  end
+  #
+  #END -------人人API----------
+
+
 end
