7c1b81344705122fc3902113dab3aea8fccd6bb2
lifang <santhe_2004@126.com>
2012-03-27 13:45:52 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PassCet4::Application.config.secret_token = 'cd8e55d229f33cb61158933110acfc26db982ddf31dcdb77a2e83c079a57565c9fadc7a55f0f0572cce04773c4fa77668531d340399b867b795f0e0361637030'
