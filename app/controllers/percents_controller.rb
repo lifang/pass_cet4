b193aea8683b81f220d2f35c620abf77a04f12d8
qianjun <er6788@126.com>
2012-03-28 16:49:21 +0800

    qianjun

--- app/controllers/percents_controller.rb
+++ app/controllers/percents_controller.rb
@@ -49,4 +49,31 @@ class PercentsController < ApplicationController
     end
   end
 
+
+  #发表微博，并转发到空间
+  def add_idol
+#    client_ip=request.headers["HTTP_X_REAL_IP"]
+    total_score = params["ability"].to_i + params["heart"].to_i + params["attitude"].to_i
+    @message = Constant::SCORE_LEVEL[total_score]
+    pars={
+      :appid=>Constant::APPID,:openid=>params[:openid].to_s,:openkey=>params[:openkey].to_s,:pf=>"",:content=>"111",:clientip=>"127.0.0.1"
+    }
+    url="http://119.147.19.43/v3/t/add_t"
+    url_params=pars.sort.map{|k,v|"#{k}=#{v}"}.join("& ")
+#    url="openid=08B8273CACFBE0D9F57CAB4E7D8AAAA0& openkey=9999999917C28878F66F28E7F16B7F89E7E752D589B8B261& appid=1& sig=9999b41ad0b688530bb1b21c5957391c& pf=tapp& format=json& userip=112.90.139.30& content=aaaa& clientip=129.10.20.11& jing=+110.5& wei=+23.4"
+    uri=URI.parse("#{url}?#{url_params}&sig=#{signature_params(Constant::APPID,url_params,url,"POST",Constant::APPKEY)}")
+    p "#{url}?#{url_params}&sig=#{signature_params(Constant::APPID,url_params,url,"POST",Constant::APPKEY)}"
+    http = Net::HTTP.new(uri.host, uri.port)
+    request = Net::HTTP::Post.new(uri.request_uri)
+    request.set_form_data({:content=>"111",:clientip=>"127.0.0.1"})
+    respond=http.request(request)
+    p respond.body
+    respond_to do |format|
+      format.json{
+        render :json=>"成功"
+      }
+    end
+  end
+
+
 end
