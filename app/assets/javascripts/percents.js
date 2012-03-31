fc7210d834ddd9cce18922536121bdd2c31c2a65
qianjun <er6788@126.com>
2012-03-31 16:49:19 +0800

    qianjun

--- app/assets/javascripts/percents.js
+++ app/assets/javascripts/percents.js
@@ -26,14 +26,13 @@ function check_message_form(web_from) {
     return false;
 }
 
-function iframe_height(height){
-    alert(height);
-    fusion2.canvas.setHeight
-    ({
-        // 可选。表示要调整的高度，不指定或指定为0则默认取当前窗口的实际高度。
-        height : height
-    });
-}
+//function iframe_height(height){
+//    fusion2.canvas.setHeight
+//    ({
+//        // 可选。表示要调整的高度，不指定或指定为0则默认取当前窗口的实际高度。
+//        height : height
+//    });
+//}
 
 function send_message(web_from) {
     var message = $(".m_text div:first").html() + $(".m_text div:last").html();
